f277eeb3f6116f9ee7a859684f2dbb371bdb54aa
ok6og <ok6og17@gmail.com>
2024-06-25 18:34:41 +0300

    AppleAndOrange Problem

---
+++ AppleAndOragne/AppleAndOragne/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AppleAndOragne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
+        }
+
+        public static void countApplesAndOranges(int s, int t, int a, int b, List<int> apples, List<int> oranges)
+        {
+            int startOfSamHouse = s;
+            int endOfSamHouse = t;
+            int appleTreePosition = a;
+            int orangeTreePosition = b;
+
+            int numOfApplesOnSamHouse = apples.Select(x => appleTreePosition + x).Count(x=> x >= startOfSamHouse && x <= endOfSamHouse);
+            int numOfOrangesOnSamHouse = oranges.Select(x => orangeTreePosition + x).Count(x => x >= startOfSamHouse && x <= endOfSamHouse);
+
+            Console.WriteLine(numOfApplesOnSamHouse);
+            Console.WriteLine(numOfOrangesOnSamHouse);
+        }
+    }
+}
