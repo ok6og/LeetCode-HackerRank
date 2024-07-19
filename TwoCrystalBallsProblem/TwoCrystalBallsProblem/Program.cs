ca9f477ec15cc026ef3692508a2bf72ca3d3977e
ok6og <ok6og17@gmail.com>
2024-07-19 13:17:39 +0300

    Leetcode

---
+++ TwoCrystalBallsProblem/TwoCrystalBallsProblem/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace TwoCrystalBallsProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] breaks = { false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true};
+            int jmpAmount = (int)Math.Floor(Math.Sqrt(breaks.Length));
+
+            int i = jmpAmount;
+
+            for (; i < breaks.Length; i += jmpAmount) 
+            {
+                if (breaks[i])
+                {
+                    break;
+                }
+
+            }
+            i -= jmpAmount;
+
+            for (int j = 0; j < jmpAmount; j++, i++)
+            {
+                if (breaks[i])
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+
+            Console.WriteLine("slab4o");
+        }
+    }
+}
