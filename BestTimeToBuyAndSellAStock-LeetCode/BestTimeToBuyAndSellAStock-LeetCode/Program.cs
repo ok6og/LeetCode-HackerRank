766123a0f5e49703772da7634178b3fe9e619047
ok6og <ok6og17@gmail.com>
2024-07-19 13:17:39 +0300

    Leetcode

---
+++ BestTimeToBuyAndSellAStock-LeetCode/BestTimeToBuyAndSellAStock-LeetCode/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace BestTimeToBuyAndSellAStock_LeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxProfit([7, 1, 5, 3, 6, 4]));
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+            int cashMoneyMade = 0;
+            int minPrice = int.MaxValue;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < minPrice)
+                {
+                    minPrice = prices[i];
+                }
+                else if (prices[i] - minPrice > cashMoneyMade)
+                {
+                    cashMoneyMade = prices[i] - minPrice;
+                }
+                
+            }
+            return cashMoneyMade;
+        }
+    }
+}
