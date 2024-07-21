66412a806be1091d55905f51ed3d96da019abd66
ok6og <ok6og17@gmail.com>
2024-07-21 15:09:13 +0300

    BubbleSort, Queue

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = [2, 0, 2, 1, 1, 0];
+
+
+            Console.WriteLine(String.Join(" ,", BubbleSort(arr)));
+        }
+
+        public static int[] BubbleSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        (arr[j], arr[j+1]) = (arr[j+1], arr[j]);
+                        //int temp = arr[j];
+                        //arr[j] = arr[j + 1];
+                        //arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ QueueImplementation/QueueImplementation/Program.cs
@@ -0,0 +1,85 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Collections.Generic;
+
+namespace QueueImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Deque();
+            queue.Deque();
+            Console.WriteLine(queue.Deque());
+        }
+
+
+    }
+
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T value)
+        {
+            this.Value = value;
+            Next = null;
+        }
+    }
+
+    public class Queue<T>
+    {
+        public Node<T>? Head { get; set; }
+        public Node<T>? Tail { get; set; }
+
+        public int Length { get; set; }
+
+        public Queue()
+        {
+            this.Head = this.Tail = null;
+            this.Length = 0;
+        }
+
+        public void Enqueue(T item)
+        {
+            Length++;
+            Node<T> newNode = new(item);
+            
+            if (Tail == null)
+            {
+                Tail = Head = newNode;
+                return;
+            }
+
+            Tail.Next = newNode;
+
+            Tail = newNode;
+
+        }
+
+        public T Deque()
+        {
+            if (this.Head == null)
+            {
+                return default(T);
+            }
+
+            Length--;
+            var head = Head;
+            this.Head = this.Head.Next;
+
+            return head.Value;
+        }
+
+        public T Peek() 
+        {
+            return this.Head.Value;
+        }
+    }
+}
