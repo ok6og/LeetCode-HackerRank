7d5c5fa98af0ed49a271c4390b631b0e9644484e
ok6og <ok6og17@gmail.com>
2024-07-22 18:59:29 +0300

    Insane code - Not Optimized

---
+++ BestTimeToBuyAStock-NeetCode/BestTimeToBuyAStock-NeetCode/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace BestTimeToBuyAStock_NeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] prices = [7, 1, 5, 3, 0, 4];
+            Console.WriteLine(MaxProfit(prices));
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+            int pointerLeftBuy = 0;
+            int pointerRightSell = 1;
+            int maxProfit = 0;
+
+            while(pointerRightSell < prices.Length)
+            {
+                if (prices[pointerLeftBuy] < prices[pointerRightSell])
+                {
+                    int profit = prices[pointerRightSell] - prices[pointerLeftBuy];
+                    maxProfit = int.Max(maxProfit, profit);
+                }
+                else
+                {
+                   pointerLeftBuy = pointerRightSell;
+                }
+                pointerRightSell++;
+
+                
+            }
+            return maxProfit;
+
+        }
+    }
+}
---
+++ LongestSubstringWithoutRepeatingCharachters-Leetcode/LongestSubstringWithoutRepeatingCharachters-Leetcode/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
+
+namespace LongestSubstringWithoutRepeatingCharachters_Leetcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Insane code - Not Optimized
+            Console.WriteLine(LengthOfLongestSubstring("dvdf"));
+        }
+        public static int LengthOfLongestSubstring(string s)
+        {
+            int longestSubstring = 0;
+            int counter = 0;
+
+            List<(char Key, int Value)> keys = new List<(char Key, int Value)>();
+        
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!keys.Exists(x=> x.Key == s[i]))
+                {
+                    keys.Add((s[i],i));
+                    counter++;
+
+                    if (longestSubstring < counter)
+                    {
+                        longestSubstring = counter;
+                    }
+                }
+                else
+                {
+                    counter = 0;
+                    var lastOccuranceOfChar = keys.FindLast(x=> x.Key == s[i]);
+                    keys.Clear();
+                    i = lastOccuranceOfChar.Value;
+                }
+            }
+            return longestSubstring;
+        }
+    }
+}
